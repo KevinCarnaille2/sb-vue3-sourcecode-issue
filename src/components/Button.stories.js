ee55f5eeb9d7dc4732ce74f34111275e32c7d2a2
KevinCarnailleMalt <kevin@malt.com>
2023-02-21 11:39:41 +0100

    Create 3 cases

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -21,7 +21,91 @@ export default {
   },
 };
 
-export const Variants = {
+
+/**
+ * CASE 1 : explicit variant call + no v-bind args
+ * - the variant property is correctly rendered in the sourcecode
+ * - the UI rendering is OK
+ * - but the default slot is not rendered in the sourcecode
+ * 
+ * Story Template : 
+ * 
+ * ```
+ *  <Button variant="primary">{{args.default}}</Button>
+    <Button variant="secondary">{{args.default}}</Button>  
+ * ```
+ */
+export const Case1 = {
+    render: (args) => ({
+        components: {Button},
+        setup() {
+            return {
+                args,
+            };
+        },
+        template: `
+            <Button variant="primary">{{args.default}}</Button>
+            <Button variant="secondary">{{args.default}}</Button>  
+        `,
+    }),
+    args: {
+        default: 'Button',
+    }
+};
+
+
+/**
+ * CASE 2 : explicit variant call + v-bind args
+ * - the variant property is not correctly rendered in the sourcecode
+ * - the UI rendering is OK
+ * - the default slot is rendered in the sourcecode but with non-required double-quotes around it
+ * - size property is correctly rendered
+ * 
+ * Story Template : 
+ * 
+ * ```
+ * <Button v-bind="args" variant="primary">{{args.default}}</Button>
+   <Button v-bind="args" variant="secondary">{{args.default}}</Button> 
+   
+   // args.size = 'small' is set
+ * ```
+ */
+ export const Case2 = {
+    render: (args) => ({
+        components: {Button},
+        setup() {
+            return {
+                args,
+            };
+        },
+        template: `
+            <Button v-bind="args" variant="primary">{{args.default}}</Button>
+            <Button v-bind="args" variant="secondary">{{args.default}}</Button>  
+        `,
+    }),
+    args: {
+        default: 'Button',
+        size: 'small',
+    }
+};
+
+/**
+ * CASE 3 : explicit variant call + direct slot usage without args
+ * - the variant property is not correctly rendered in the sourcecode
+ * - the UI rendering is OK
+ * - the default slot is not rendered in the sourcecode
+ * 
+ *  There is as well a Vue warning : 
+ * "Extraneous non-props attributes (default) were passed to component but could not be automatically inherited because component renders fragment or text root nodes." 
+ * 
+ * Story Template : 
+ * 
+ * ```
+<Button variant="primary">Button</Button>  
+ <Button variant="secondary">Button</Button>  
+ * ```
+ *  */
+export const Case3 = {
     render: (args) => ({
         components: {Button},
         setup() {
